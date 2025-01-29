d3c99765e32bae187b9c7561b2c0ff8ce41528a5
Bazch <mikael.vidal@gmail.com>
2025-01-29 00:52:53 -0300

    Replace HomePage with WeatherHomePage and integrate WeatherController for daily weather display

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:clients_manager/src/features/weather/ui/controllers/weather_controller.dart';
+import 'package:clients_manager/src/features/weather/data/services/weather_service.dart';
+import 'package:clients_manager/src/features/weather/ui/widgets/weather_card_widget.dart';
+import 'package:clients_manager/src/features/weather/data/repositories/weather_repository.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:clients_manager/src/features/home/home_page.dart';
 
 Future<void> main() async {
   await Supabase.initialize(
@@ -12,14 +15,66 @@ Future<void> main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Supabase App',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const HomePage(),
+      debugShowCheckedModeBanner: false,
+      home: const WeatherHomePage(),
+    );
+  }
+}
+
+class WeatherHomePage extends StatefulWidget {
+  const WeatherHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<WeatherHomePage> createState() => _WeatherHomePageState();
+}
+
+class _WeatherHomePageState extends State<WeatherHomePage> {
+  late final WeatherController _weatherController;
+
+  @override
+  void initState() {
+    super.initState();
+    _weatherController = WeatherController(
+      weatherService: WeatherService(repository: WeatherRepository(apiKey: '67241a63d0acb68d90993bd49c549327')),
+    );
+    
+    _weatherController.fetchDailyWeather(
+      latitude: 37.0154,
+      longitude: -7.9352,
+      lang: 'en',
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Weather"),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              "Weather situation for Faro today",
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.blueAccent,
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            WeatherCard(controller: _weatherController),
+          ],
+        ),
+      ),
     );
   }
 }
