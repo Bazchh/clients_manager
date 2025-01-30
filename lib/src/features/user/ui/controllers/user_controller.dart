c3f7ae92a8d73be79209e0c9d0cbba84441f4ae5
Bazch <mikael.vidal@gmail.com>
2025-01-30 00:17:51 -0300

    Refactor user controller methods to improve error handling and logging during user creation, deletion, and editing

--- lib/src/features/user/ui/controllers/user_controller.dart
+++ lib/src/features/user/ui/controllers/user_controller.dart
@@ -31,42 +31,44 @@ class UserController extends ChangeNotifier {
   }
 
   Future<bool> createUser(ExtendedUser newUser, String password) async {
-  try {
-    await userService.createUser(newUser, password);
-    _users.add(newUser);
-    notifyListeners();
-    return true; 
-  } catch (e) {
-    _errorMessage = 'Failed to create user: $e';
-    notifyListeners();
-    return false; 
+    try {
+      await userService.createUser(newUser, password);
+      _users.add(newUser);
+      notifyListeners();
+      return true;
+    } catch (e) {
+      _errorMessage = 'Failed to create user: $e';
+      notifyListeners();
+      return false;
+    }
   }
-}
 
   Future<bool> deleteUser(String userId) async {
-  try {
-    await userService.deleteUser(userId);
-    _users.removeWhere((user) => user.id == userId);
-    notifyListeners();
-    return true; 
-  } catch (e) {
-    _errorMessage = 'Failed to delete user: $e';
-    notifyListeners();
-    return false; 
+    try {
+      await userService.deleteUser(userId);
+      _users.removeWhere((user) => user.id == userId);
+      notifyListeners();
+      return true;
+    } catch (e) {
+      _errorMessage = 'Failed to delete user: $e';
+      notifyListeners();
+      return false;
+    }
   }
-}
-
 
   Future<void> editUser(ExtendedUser updatedUser) async {
     try {
+      print('Attempting to update user: ${updatedUser.id}');
       await userService.updateUser(updatedUser);
-      final index =
-          _users.indexWhere((user) => user.id == updatedUser.id);
+      print('User updated successfully: ${updatedUser.id}');
+
+      final index = _users.indexWhere((user) => user.id == updatedUser.id);
       if (index != -1) {
         _users[index] = updatedUser;
         notifyListeners();
       }
     } catch (e) {
+      print('Error updating user: $e'); // Log detalhado do erro
       _errorMessage = 'Failed to edit user: $e';
       notifyListeners();
     }
