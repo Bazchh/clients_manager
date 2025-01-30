6ae034e1ab3f1d30538346bd566e5da89375078f
Bazch <mikael.vidal@gmail.com>
2025-01-30 02:04:33 -0300

    Refactor WeatherService to fetch current weather data and return it in a formatted list

--- lib/src/features/weather/data/services/weather_service.dart
+++ lib/src/features/weather/data/services/weather_service.dart
@@ -6,19 +6,21 @@ class WeatherService {
 
   WeatherService({required this.repository});
 
-  Future<List<DailyWeather>> getFormattedDailyWeather({
+  Future<List<CurrentWeather>> getFormattedDailyWeather({
     required double latitude,
     required double longitude,
     String lang = 'en',
   }) async {
     try {
-      final dailyWeatherList = await repository.fetchDailyWeather(
+      // Busca os dados atuais do clima
+      final currentWeather = await repository.fetchCurrentWeather(
         latitude: latitude,
         longitude: longitude,
         lang: lang,
       );
 
-      return dailyWeatherList;
+      // Retorna os dados formatados como uma lista (para compatibilidade futura)
+      return [currentWeather];
     } catch (e) {
       throw Exception('Erro ao processar os dados do clima: $e');
     }
