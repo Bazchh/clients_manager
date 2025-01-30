e51416baa75e8a2229d60ae618dd736073dad676
Bazch <mikael.vidal@gmail.com>
2025-01-30 02:04:37 -0300

    Refactor DailyWeather model to CurrentWeather and update properties for current weather data

--- lib/src/features/weather/domain/models/weather_model.dart
+++ lib/src/features/weather/domain/models/weather_model.dart
@@ -1,46 +1,28 @@
-class DailyWeather {
+class CurrentWeather {
   final DateTime date;
-  final double minTemp;
-  final double maxTemp;
-  final double dayTemp;
-  final double nightTemp;
+  final double tempMin;
+  final double tempMax;
   final String weatherDescription;
   final String weatherIcon;
   final int humidity;
-  final double windSpeed;
-  final int windDirection;
-  final double uvi;
-  final double precipitationProbability;
 
-  DailyWeather({
+  CurrentWeather({
     required this.date,
-    required this.minTemp,
-    required this.maxTemp,
-    required this.dayTemp,
-    required this.nightTemp,
+    required this.tempMin,
+    required this.tempMax,
     required this.weatherDescription,
     required this.weatherIcon,
     required this.humidity,
-    required this.windSpeed,
-    required this.windDirection,
-    required this.uvi,
-    required this.precipitationProbability,
   });
 
-  factory DailyWeather.fromJson(Map<String, dynamic> json) {
-    return DailyWeather(
+  factory CurrentWeather.fromJson(Map<String, dynamic> json) {
+    return CurrentWeather(
       date: DateTime.fromMillisecondsSinceEpoch(json['dt'] * 1000),
-      minTemp: json['temp']['min'].toDouble(),
-      maxTemp: json['temp']['max'].toDouble(),
-      dayTemp: json['temp']['day'].toDouble(),
-      nightTemp: json['temp']['night'].toDouble(),
+      tempMin: json['main']['temp_min'].toDouble(),
+      tempMax: json['main']['temp_max'].toDouble(),
       weatherDescription: json['weather'][0]['description'],
       weatherIcon: json['weather'][0]['icon'],
-      humidity: json['humidity'],
-      windSpeed: json['wind_speed'].toDouble(),
-      windDirection: json['wind_deg'],
-      uvi: json['uvi'].toDouble(),
-      precipitationProbability: (json['pop'] * 100), 
+      humidity: json['main']['humidity'],
     );
   }
 }
