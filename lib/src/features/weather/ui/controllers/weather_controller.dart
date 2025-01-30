7db05760c2cf2878a611ee73bf0a1e307178936c
Bazch <mikael.vidal@gmail.com>
2025-01-30 02:04:42 -0300

    Refactor WeatherController to use CurrentWeather model and update fetch method for improved clarity

--- lib/src/features/weather/ui/controllers/weather_controller.dart
+++ lib/src/features/weather/ui/controllers/weather_controller.dart
@@ -1,33 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:clients_manager/src/features/weather/domain/models/weather_model.dart';
 import 'package:clients_manager/src/features/weather/data/services/weather_service.dart';
+import 'package:clients_manager/src/features/weather/domain/models/weather_model.dart';
 
 class WeatherController {
-  final WeatherService weatherService;
-  final ValueNotifier<List<DailyWeather>?> dailyWeatherNotifier = ValueNotifier(null);
+  final WeatherService service;
   final ValueNotifier<bool> isLoading = ValueNotifier(false);
   final ValueNotifier<String?> errorMessage = ValueNotifier(null);
+  final ValueNotifier<List<CurrentWeather>?> weatherListNotifier = ValueNotifier(null);
 
-  WeatherController({required this.weatherService});
+  WeatherController({required this.service});
 
-  Future<void> fetchDailyWeather({
-    required double latitude,
-    required double longitude,
-    String lang = 'en',
-  }) async {
+  Future<void> fetchWeather(double latitude, double longitude) async {
+    isLoading.value = true;
+    errorMessage.value = null;
+    weatherListNotifier.value = null;
+
     try {
-      isLoading.value = true;
-      errorMessage.value = null;
-
-      final dailyWeatherList = await weatherService.getFormattedDailyWeather(
+      // Usa o serviço para buscar os dados formatados
+      final weatherList = await service.getFormattedDailyWeather(
         latitude: latitude,
         longitude: longitude,
-        lang: lang,
       );
 
-      dailyWeatherNotifier.value = dailyWeatherList;
+      // Atualiza o notificador com a lista de dados
+      weatherListNotifier.value = weatherList;
     } catch (e) {
-      errorMessage.value = 'Erro ao buscar os dados meteorológicos: $e';
+      errorMessage.value = 'Erro ao buscar clima: $e';
     } finally {
       isLoading.value = false;
     }
