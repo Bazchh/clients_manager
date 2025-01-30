8e1b2ceebb14627b4fc01824aaec18a24e9ec72b
Bazch <mikael.vidal@gmail.com>
2025-01-30 02:04:26 -0300

    Refactor WeatherRepository to fetch current weather data and improve error handling

--- lib/src/features/weather/data/repositories/weather_repository.dart
+++ lib/src/features/weather/data/repositories/weather_repository.dart
@@ -8,34 +8,38 @@ class WeatherRepository {
 
   WeatherRepository({
     required this.apiKey,
-    this.apibaseUrl = 'https://api.openweathermap.org/data/3.0/onecall',
+    this.apibaseUrl = 'https://api.openweathermap.org/data/2.5/weather',
   });
 
-  Future<List<DailyWeather>> fetchDailyWeather({
+  Future<CurrentWeather> fetchCurrentWeather({
     required double latitude,
     required double longitude,
-    String exclude = 'current,minutely,hourly,alerts',
     String units = 'metric',
     String lang = 'en',
   }) async {
     final url = Uri.parse(
-      '$apibaseUrl?lat=$latitude&lon=$longitude&exclude=$exclude&units=$units&lang=$lang&appid=$apiKey',
+      '$apibaseUrl?lat=$latitude&lon=$longitude&units=$units&lang=$lang&appid=$apiKey',
     );
 
+    // Log da URL gerada
     print('Requisição para: $url');
 
     try {
+      // Realiza a requisição HTTP
       final response = await http.get(url);
 
+      // Log do status code da resposta
+      print('Status Code: ${response.statusCode}');
+
+      // Verifica se a resposta foi bem-sucedida (status code 200)
       if (response.statusCode == 200) {
         final data = json.decode(response.body);
-        final List<dynamic> dailyJson = data['daily'];
-
-        return dailyJson.map((json) => DailyWeather.fromJson(json)).toList();
+        return CurrentWeather.fromJson(data);
       } else {
         throw Exception('${response.reasonPhrase}');
       }
     } catch (error) {
+      print('Erro capturado: $error');
       throw Exception('$error');
     }
   }
